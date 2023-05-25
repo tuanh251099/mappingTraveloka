86d108ba16abf411424ae25d5ca4d095fa7d345f
Tuanh <tuanh.2510.ccpr@gmail.com>
2023-05-25 20:50:26 +0700

    update at home 25/05

--- mappingHotel/lstHotelCountry/lstCity.js
+++ mappingHotel/lstHotelCountry/lstCity.js
@@ -1,26 +1,48 @@
 const fs = require('fs');
 const _ = require('lodash');
 
-
-const rawData = fs.readFileSync('hotels_Traveloka_Philippines.json');
+const rawData = fs.readFileSync('hotels_Traveloka_Vietnam.json');
 const data = JSON.parse(rawData);
+console.log(data.length)
+// const cityCounts = {};
 
+// _.forEach(data, (element) => {
+//   const City = element.City;
 
-const cityCounts = {};
+//   if (_.has(cityCounts, City)) {
+//     cityCounts[City] += 1;
+//   } else {
+//     cityCounts[City] = 1;
+//   }
+// });
 
+// // Tạo mảng các đối tượng {city, count}
+// const cityCountArray = _.map(cityCounts, (count, City) => ({ city: City, count }));
 
-_.forEach(data, (element) => {
-  const City = element.City;
+// // Sắp xếp theo tên thành phố
+// const sortedCityCountArray = _.sortBy(cityCountArray, 'city');
 
+// // Tạo dòng header
+// const header = 'City,Count\n';
 
-  if (_.has(cityCounts, City)) {
-    cityCounts[City] += 1;
-  } else {
-    cityCounts[City] = 1;
-  }
-});
+// // Tạo dữ liệu CSV
+// let csvData = '';
 
+// // Thêm dòng header vào chuỗi CSV
+// csvData += header;
 
-_.forEach(cityCounts, (count, City) => {
-  console.log(`City: ${City}, Count: ${count}`);
-});
+// _.forEach(sortedCityCountArray, (cityCount) => {
+//   // Bao quanh giá trị thành phố bằng dấu ngoặc kép
+//   const cityValue = `"${cityCount.city}"`;
+
+//   // Thêm dòng vào chuỗi CSV
+//   csvData += `${cityValue},${cityCount.count}\n`;
+// });
+
+// // Tên tệp CSV mới
+// const csvFileName = 'city_Singapore.csv';
+
+// // Ghi dữ liệu vào tệp CSV
+// fs.writeFileSync(csvFileName, csvData);
+
+// console.log(`Dữ liệu đã được lưu vào tệp ${csvFileName}`);
